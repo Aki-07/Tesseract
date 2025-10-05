189618ce9f6a1c0ba027e7b5b0bc6b69882502ab
Aki-07 <akileshramesh2003@gmail.com>
2025-10-06 00:17:01 +0530

    add service endpoint

--- services/ui/src/app/utils/api.ts
+++ services/ui/src/app/utils/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 console.log("NEXT_PUBLIC_API_URL at runtime:", process.env.NEXT_PUBLIC_API_URL);
 
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL
-});
+const api =
+  process.env.NEXT_PUBLIC_API_URL || "https://tesseract-ptnh.onrender.com";
+
 console.log("NEXT_PUBLIC_API_URL at runtime:", api);
 export default api;
